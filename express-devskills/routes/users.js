b16e503c813d40bf2878a8114b79def0f59ebc55
WomackCodes <chris@womack.codes>
2020-08-18 14:24:41 -0700

    Complete up to to-do refactor on class notes

---
+++ express-devskills/routes/users.js
@@ -0,0 +1,12 @@
+// This file is an example of a router dedicated 
+// to a resource, in this case, users.
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
