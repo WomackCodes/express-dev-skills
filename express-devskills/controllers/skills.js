58799096bcd1cad4189cd0a42b1b631ce1451ea2
WomackCodes <chris@womack.codes>
2020-08-20 14:07:03 -0700

    CSS Changes

--- express-devskills/controllers/skills.js
+++ express-devskills/controllers/skills.js
@@ -23,7 +23,7 @@ function index(req, res) {
 function show(req, res) {
     res.render("skills/show", {
         skill: Skill.getOne(req.params.id),
-        // Would like to display the number of the todo within the list
+        // Would like to display the number of the  within the list
         skillNum:
             Skill.getAll().findIndex(
                 (skill) => skill.id === parseInt(req.params.id)
